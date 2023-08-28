79d579dcf8c13fc983684027baec0083e78beb59
TJCole03 <tjc6254@gmail.com>
2023-08-27 20:05:08 -0400

    sample user creation test in app.test.js

---
+++ tests/app.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest')
+const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server')
+const app = require('../app.js')
+const PORT = process.env.PORT || 3002
+const server = app.listen(`${PORT}`, () => console.log('Testing on PORT'))
+const Tomato = require('../models/tomato.js')
+const User = require('../models/user.js')
+let mongoServer; 
+
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create()
+    console.log(mongoServer.getUri()) //ask Jeff about whether it's URL or URI for this project
+    await mongoose.connect(mongoServer.getUri())
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+    mongoServer.stop()
+    server.close()
+})
+
+describe('Testing Endpoints', () => {
+
+    test('Creating User', async () => {
+        const response = await request(app) 
+            .post('/user') //ACTUAL ENDPOINT NAME PENDING
+            .send({
+                userName: "checking",
+                email: "checking",
+                password: "checking",
+                address: "checking",
+                paymentInfo: "checking"
+            })
+        expect(response.statusCode).toBe(200)
+        console.log(response.body)
+        expect(response.body.newUser.userName).toBe("checking")
+        expect(response.body.newUser.email).toBe("checking")
+        expect(response.body.newUser.password).toBe("checking")
+        expect(response.body.newUser.address).toBe("checking")
+        expect(response.body.newUser.paymentInfo).toBe("checking")
+        expect(response.body).toHaveProperty('token')
+    })
+})
